43395e3acc4fd1512ac2f77b88fee5190312ec6d
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-08 15:50:19 +0100

    update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Route from "./route/Route";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(<Route />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
