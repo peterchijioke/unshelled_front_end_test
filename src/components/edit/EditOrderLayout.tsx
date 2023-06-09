35b4794d43c1a92ccbb8dac54f3605810ffed205
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-09 17:40:42 +0100

    update

--- src/components/edit/EditOrderLayout.tsx
+++ src/components/edit/EditOrderLayout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./order.css";
+import "./edit_order.css";
 import { OrderInterface } from "../../type";
 
 export default function EditOrderLayout({ ...props }) {
@@ -7,22 +7,28 @@ export default function EditOrderLayout({ ...props }) {
   return (
     <div className="order-layout-wrapper">
       <div className="card-order">
-        <h1>Order Details</h1>
-        <div className="wrap-txt">
-          <span> Order ID: {item.id}</span>
-        </div>
-        <div className="wrap-txt">
-          <span> Date: {item.date}</span>
-        </div>
-        <div className="wrap-txt">
-          <span> Product Price: {item.price}</span>
-        </div>
-        <div className="wrap-txt">
-          <span> Product Category: {item.product_category}</span>
-        </div>
-        <div className="wrap-txt">
-          <span> Product ID: {item.product_id}</span>
-        </div>
+        <h1>Order Details Edit</h1>
+
+        <input
+          value={item.price}
+          placeholder="price"
+          className="edit-order-input"
+          type="text"
+        />
+        <input
+          value={item.product_category}
+          placeholder="Category"
+          className="edit-order-input"
+          type="text"
+        />
+        <input
+          value={item.date}
+          placeholder="Date"
+          className="edit-order-input"
+          type="date"
+        />
+
+        <input className="save-btn" type="submit" value="Save " />
       </div>
     </div>
   );
