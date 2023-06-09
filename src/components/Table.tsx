a2af86bccb93d8a78149aaad68585651a39f74b8
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-09 10:05:16 +0100

    update

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,15 +3,18 @@ import "./table.css";
 import { getOrderList } from "../services/Services";
 import { useNavigate } from "react-router-dom";
 import { OrderInterface } from "../type";
+import LoadingElement from "./login/LoadingElement";
 
 export default function Table() {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
+  const [progress, setProgress] = useState(false);
   const getOrderdList = async () => {
+    setProgress(!progress);
     const res: Response | any = await getOrderList(1, 10);
     if (res) {
-      console.log(res.data);
       setData(res.data.data);
+      setProgress(!progress);
     }
   };
   useEffect(() => {
@@ -23,6 +26,15 @@ export default function Table() {
   };
   const goToDetails = (item: OrderInterface) => {
     navigate(`/order/${item.id}`, { state: { item } });
+  };
+  const handlePrev = () => {};
+  const handleNext = () => {
+    setProgress(!progress);
+    const res: Response | any = await getOrderList(1, 10);
+    if (res) {
+      setData(res.data.data);
+      setProgress(!progress);
+    }
   };
   return (
     <div
@@ -44,19 +56,33 @@ export default function Table() {
             <th>Date</th>
             <th>Product Category</th>
             <th>Price</th>
+            <th>Action</th>
           </tr>
         </thead>
-        <tbody>
-          {data.map((item: OrderInterface) => (
-            <tr onClick={() => goToDetails(item)}>
-              <td>{item.id}</td>
-              <td>{item.product_id}</td>
-              <td>{item.date}</td>
-              <td>{item.product_category}</td>
-              <td>{item.price}</td>
-            </tr>
-          ))}
-        </tbody>
+        {progress && !data.length ? (
+          <>
+            <LoadingElement />
+          </>
+        ) : (
+          <tbody>
+            {data.map((item: OrderInterface) => (
+              <tr onClick={() => goToDetails(item)}>
+                <td>{item.id}</td>
+                <td>{item.product_id}</td>
+                <td>{item.date}</td>
+                <td>{item.product_category}</td>
+                <td>{item.price}</td>
+                <td className="delete-btn">Delete</td>
+              </tr>
+            ))}
+          </tbody>
+        )}
+        <tfoot>
+          <div className="btn-wrap">
+            <button onClick={handlePrev}>Previous</button>{" "}
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </tfoot>
       </table>
     </div>
   );
